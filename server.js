eff2ad89597df0182e1c6a12a096484ec4677e2a
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-08-01 19:39:16 +0530

    commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3012;
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
